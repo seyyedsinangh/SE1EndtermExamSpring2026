225c720db99a955cd31e608302a9fe127e090dbb
seyyed <sina.seyyed.2004@gmail.com>
2026-06-30 11:04:05 +0330

    Test Created and Tested

--- 40131055/property-service/src/test/java/edu/university/s40131055/property/PropertyServiceApplicationTests.java
+++ 40131055/property-service/src/test/java/edu/university/s40131055/property/PropertyServiceApplicationTests.java
@@ -1,7 +1,17 @@
 package edu.university.s40131055.property;
 
+import edu.university.s40131055.property.service.PropertyService;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 public class PropertyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+        PropertyService service = new PropertyService();
+        assertNotNull(service);
+    }
 }
